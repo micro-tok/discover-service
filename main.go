8421c12f54d11414160150a5d1807c33f1042a79
Martin Bozhilov <marogo142005@gmail.com>
2024-04-24 21:08:26 +0300

    [ADD] service impl

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"net"
 
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/micro-tok/discover-service/pkg/cassandra"
 	"github.com/micro-tok/discover-service/pkg/config"
+	"github.com/micro-tok/discover-service/pkg/pb"
+	"github.com/micro-tok/discover-service/pkg/redis"
+	"github.com/micro-tok/discover-service/pkg/services"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+
+	cassService := cassandra.NewCassandraService(cfg)
+
+	redisService := redis.NewRedisService(cfg)
 
 	lis, err := net.Listen("tcp", "localhost:"+cfg.ServicePort)
 	if err != nil {
@@ -19,6 +27,9 @@ func main() {
 	grpcServer := grpc.NewServer(
 		grpc.MaxRecvMsgSize(1024 * 1024 * 1024 * 2),
 	)
+
+	// Register the service
+	pb.RegisterDiscoverServiceServer(grpcServer, services.NewDiscoverService(cassService, redisService))
 
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
